89c590774331812886af86bdd3e328117066062f
TitaniumTurbine <63096937+tom-broadbent@users.noreply.github.com>
2025-01-14 15:30:36 +0000

    started on symbol picker

---
+++ LatexEditor/Views/SymbolPicker.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LatexEditor.Views;
+
+public partial class SymbolPicker : Window
+{
+    public SymbolPicker()
+    {
+        InitializeComponent();
+    }
+}
