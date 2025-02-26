e1158fb3e08eda117f3ab8ad72d4488543e48db9
TitaniumTurbine <63096937+tom-broadbent@users.noreply.github.com>
2025-02-26 13:34:52 +0000

    Add functionality to macro menu

--- LatexEditor/Views/MacroMenu.axaml.cs
+++ LatexEditor/Views/MacroMenu.axaml.cs
@@ -5,6 +5,7 @@ using Avalonia.Data;
 using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
 using LatexEditor.ViewModels;
+using LatexEditor.Views;
 using Newtonsoft.Json;
 using System;
 using System.Collections.ObjectModel;
@@ -43,6 +44,62 @@ public partial class MacroMenu : Window
             var json = JsonConvert.SerializeObject(vm.Macros);
             File.WriteAllText("Macros.json", json);
             Close();
+        };
+
+        AddButton.Click += (s, e) =>
+        {
+            if (Owner is MainWindow window)
+            {
+                var macro = MacroGrid.SelectedItem as LatexMacro;
+                if (macro != null)
+                {
+                    var paramCount = macro.Latex?.Count(f => f == '#') ?? 0;
+                    var insertText = $"\\newcommand{{\\{macro.CommandName}}}";
+                    if (paramCount > 0)
+                    {
+                        insertText += $"[{paramCount}]";
+                    }
+                    insertText += $"{{{macro.Latex}}}\n";
+
+                    var doc = window.textEditor.Document;
+                    doc.Insert(0, insertText);
+                    window.SetChangeMarker();
+                }
+            }
+        };
+
+        InsertButton.Click += (s, e) =>
+        {
+            if (Owner is MainWindow window)
+            {
+                var macro = MacroGrid.SelectedItem as LatexMacro;
+                if (macro != null)
+                {
+                    var doc = window.textEditor.Document;
+                    var offset = window.textEditor.CaretOffset;
+                    doc.Insert(offset, macro.Latex);
+                    window.SetChangeMarker();
+                }
+            }
+        };
+
+        DuplicateButton.Click += (s, e) =>
+        {
+            var macro = MacroGrid.SelectedItem as LatexMacro;
+            if (macro != null)
+            {
+                var index = vm.Macros.IndexOf(macro);
+                vm.Macros.Insert(index, new LatexMacro() { Name = macro.Name, Latex = macro.Latex, CommandName = macro.CommandName });
+            }
+        };
+
+        DeleteButton.Click += (s, e) =>
+        {
+            var macro = MacroGrid.SelectedItem as LatexMacro;
+            if (macro != null)
+            {
+                vm.Macros.Remove(macro);
+            }
         };
     }
 
