f24dd15abd0cc70c64e2875b7ee7d3c91b52835a
TitaniumTurbine <63096937+tom-broadbent@users.noreply.github.com>
2025-03-23 12:19:19 +0000

    Add multi text entry window

---
+++ LatexEditor/ViewModels/EnterMultiTextDialogViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatexEditor.ViewModels
+{
+    public partial class EnterMultiTextDialogViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private ObservableCollection<string> textBoxWatermarks = new();
+
+        [ObservableProperty]
+        private ObservableCollection<string> textBoxDefaultTexts = new();
+    }
+}
---
+++ LatexEditor/Views/EnterMultiTextDialog.axaml.cs
@@ -0,0 +1,59 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using LatexEditor.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LatexEditor.Views;
+
+public partial class EnterMultiTextDialog : Window
+{
+    private List<TextBox> textBoxes = new();
+    public EnterMultiTextDialog(int textBoxCount = 1)
+    {
+        if( textBoxCount < 1)
+        {
+            throw new ArgumentOutOfRangeException("textBoxCount must be greater than or equal to 1");
+        }
+
+        InitializeComponent();
+        var vm = DataContext as EnterMultiTextDialogViewModel;
+
+        CancelButton.Click += CancelButton_Click;
+        OkButton.Click += OkButton_Click;
+
+
+
+        for (int i = 0; i < textBoxCount; i++)
+        {
+            var textBox = new TextBox()
+            {
+                Watermark = vm.TextBoxWatermarks.Count >= i ? vm.TextBoxWatermarks[i] : null,
+                Text = vm.TextBoxDefaultTexts.Count >= i ? vm.TextBoxDefaultTexts[i] : null,
+                [Grid.RowProperty] = i,
+                [Grid.ColumnSpanProperty] = 2,
+            };
+            textBoxes.Add(textBox);
+            MainGrid.Children.Add(textBox);
+
+            MainGrid.RowDefinitions.Add(new RowDefinition(GridLength.Auto));
+        }
+
+        CancelButton[Grid.RowProperty] = MainGrid.RowDefinitions.Count;
+        MainGrid.RowDefinitions.Add(new RowDefinition(GridLength.Auto));
+        OkButton[Grid.RowProperty] = MainGrid.RowDefinitions.Count;
+        MainGrid.RowDefinitions.Add(new RowDefinition(GridLength.Auto));
+    }
+
+    private void CancelButton_Click(object? sender, EventArgs e)
+    {
+        Close();
+    }
+
+    private void OkButton_Click(object? sender, EventArgs e)
+    {
+        Close(textBoxes.Select(box => box.Text));
+    }
+}
